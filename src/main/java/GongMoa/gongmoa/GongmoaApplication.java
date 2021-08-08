6b0e994bde1d5147418944ea781eecc549a76032
Chan Kim <chankim@Chanui-MacBookPro.local>
2021-08-08 16:43:28 +0900

    first

---
+++ src/main/java/GongMoa/gongmoa/GongmoaApplication.java
@@ -0,0 +1,13 @@
+package GongMoa.gongmoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GongmoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GongmoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GongMoa/gongmoa/GongmoaApplicationTests.java
@@ -0,0 +1,13 @@
+package GongMoa.gongmoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongmoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
